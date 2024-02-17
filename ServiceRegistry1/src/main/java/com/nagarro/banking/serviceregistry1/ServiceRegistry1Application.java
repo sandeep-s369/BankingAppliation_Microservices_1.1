f76dc6e97255bcf9305e4b244f0cce6712c306f3
Sandeep Salla <156847393+sandeep-s369@users.noreply.github.com>
2024-02-17 13:49:05 +0530

    Add files via upload
    Spring Boot Application acts as a Service Registry for this respected Microservices Banking Application

---
+++ ServiceRegistry1/src/main/java/com/nagarro/banking/serviceregistry1/ServiceRegistry1Application.java
@@ -0,0 +1,21 @@
+package com.nagarro.banking.serviceregistry1;
+
+import org.springframework.boot.SpringApplication;
+
+
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ServiceRegistry1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistry1Application.class, args);
+	}
+
+}
+ 
